728281e3eb943b0ab9b826a27b5bb18c19f189f5
Masahiro Kisono <mkisono@gmail.com>
2023-11-23 15:00:28 +0900

    rename component name

--- template/setup.py
+++ template/setup.py
@@ -6,11 +6,11 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
-    name="streamlit-custom-component",
+    name="streamlit-backgammon",
     version="0.0.1",
-    author="John Smith",
-    author_email="john@example.com",
-    description="Streamlit component that allows you to do X",
+    author="Masahiro Kisono",
+    author_email="mkisono@gmail.com",
+    description="Streamlit component that allows you to render backgammon position",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
