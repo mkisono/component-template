67ba12f39a3bd6a13c7ca3cde96099db5ffbc51b
Masahiro Kisono <mkisono@gmail.com>
2023-12-04 21:02:18 +0900

    Update player win and gammon percentage display

--- slider-with-buttons/slider_with_buttons/example.py
+++ slider-with-buttons/slider_with_buttons/example.py
@@ -4,5 +4,5 @@ from slider_with_buttons import slider
 player_win = slider("Player Win %", 50, key="player_win")
 player_g = slider("Player Gammon %", 0, key="player_g")
 
-st.write("Player Win %", st.session_state.player_win)
-st.write("Player Gammon %", st.session_state.player_g)
+st.write("Player Win %", player_win)
+st.write("Player Gammon %", player_g)
